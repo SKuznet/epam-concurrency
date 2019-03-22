071cba3be94603fceb22a55f3e464d9e9c6c16bf
Артем <b>
2019-03-22 22:52:39 +0300

    added first homework

--- src/main/java/com/epam/concurrency/lesson2/ATM.java
+++ src/main/java/com/epam/concurrency/lesson2/ATM.java
@@ -3,23 +3,21 @@ package com.epam.concurrency.lesson2;
 import java.util.concurrent.TimeUnit;
 
 public class ATM {
-    // вся сумма в банкомате
     private static int money = 100;
 
-    // принцип туалета с ключом
-    private synchronized static void getMoney(int amount) {
-
+    private static synchronized void getMoney(int amount) {
         if (amount <= money) {
             try {
                 TimeUnit.MILLISECONDS.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            money-=amount;
+            money -= amount;
             System.err.println("ALL OK! New balance: " + money);
-        } else {
-            System.err.println("No enough money!");
+        }else {
+            System.err.println("Not enough money");
         }
+
     }
 
     public static void main(String[] args) {
@@ -44,5 +42,7 @@ public class ATM {
                 getMoney(50);
             }
         }).start();
+
     }
 }
+
