13f66c51142252091f8465cc4203c9c90677a89d
Lev Kiselev <lev_kiselev@epam.com>
2019-03-23 10:58:18 +0300

    Add first homework

---
+++ src/main/java/com/epam/concurrency/homework/HW1/HW1.java
@@ -0,0 +1,49 @@
+package com.epam.concurrency.homework.HW1;
+
+
+import java.util.concurrent.TimeUnit;
+
+public class HW1 {
+
+    private static int money = 100;
+
+    private synchronized static void getMoney(int amount) {
+        if (amount <= money) {
+            try {
+                TimeUnit.MILLISECONDS.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            money -= amount;
+            System.err.println("All OK! New balance: " + money);
+        } else {
+            System.err.println("no enough money");
+        }
+    }
+
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.err.println("firstName");
+                getMoney(50);
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.err.println("secondName");
+                getMoney(50);
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.err.println("thirdName");
+                getMoney(50);
+            }
+        }).start();
+    }
+}
