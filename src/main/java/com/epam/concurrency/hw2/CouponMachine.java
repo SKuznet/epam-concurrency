6181f012c6055e23a6b350f931d4235386230793
Olga Kochneva <kochnevaolga74@bk.ru>
2019-04-12 02:19:59 +0300

    Free donuts cafe

--- src/main/java/com/epam/concurrency/hw2/CouponMachine.java
+++ src/main/java/com/epam/concurrency/hw2/CouponMachine.java
@@ -5,11 +5,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class CouponMachine {
     private AtomicInteger couponNumber = new AtomicInteger(0);
 
-
     public int getCoupon() {
-        System.out.println("Coupon #" + couponNumber.get() +1);
+        //System.out.println("Coupon #" + couponNumber.get() +1);
         return couponNumber.getAndAdd(1);
     }
-
-
 }
--- src/main/java/com/epam/concurrency/hw2/FreeDonutsCafe.java
+++ src/main/java/com/epam/concurrency/hw2/FreeDonutsCafe.java
@@ -1,8 +1,40 @@
 package com.epam.concurrency.hw2;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class FreeDonutsCafe {
 
     public static void main(String[] args) {
+        CouponMachine couponMachine = new CouponMachine();
+        Set<Integer> freeDonuts = new HashSet<>();
 
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for (int i = 0; i < 10; i++) {
+            executorService.execute(() -> {
+                while (true) {
+                    int newCoupon = couponMachine.getCoupon();
+                    if (!freeDonuts.add(newCoupon)) {
+                        System.err.printf("Coupon #%d is not valid\n", newCoupon);
+                        System.exit(1);
+                    }
+                }
+            });
+        }
+
+        executorService.execute(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.err.println("Free-Donuts cafe is closed. We have donated " + freeDonuts.size() + " donuts");
+            System.exit(1);
+        });
+        executorService.shutdown();
     }
 }
